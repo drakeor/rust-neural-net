e648402a15636e00030aa5ee4a6830decd81e062
Drakeor <drakeor.dragon@gmail.com>
2018-05-28 02:07:29 -0700

    Finished second part of the rust neural net code

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,13 @@ pub fn sigmoid(z : f32) -> f32 {
     1_f32 / ( 1_f32 + (-1_f32 * z).exp())
 }
 
-pub fn sigmoid_mat(matrix: Matrix<f32>) -> Matrix<f32> {
-    let matrix_b = matrix.apply(&sigmoid);
-    matrix_b
-}
-
 impl NeuralNetwork {
-    pub fn forward(&mut self, matrix : Matrix<f32>)
+    pub fn forward(&mut self, matrix : Matrix<f32>) -> Matrix<f32>
     {
-        
+        let z2 = matrix * &self.w1;
+        let a2 = z2.apply(&sigmoid);
+        let z3 = a2 * &self.w2;
+        z3.apply(&sigmoid)
     }
 }
 
@@ -36,8 +34,16 @@ fn main() {
         w1: Matrix::new(2, 3, vec![
             rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>(), 
             rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>() ]),
-        w2: Matrix::new(1, 3, vec![
+        w2: Matrix::new(3, 1, vec![
             rand::random::<f32>(), rand::random::<f32>(), rand::random::<f32>()                
         ]),
     };
+    
+    let X = Matrix::new(3, 2, vec![
+        3_f32, 5_f32, 
+        5_f32, 1_f32, 
+        10_f32, 2_f32]);
+
+    let yhat = network.forward(X);
+    println!("{}", yhat);
 }
